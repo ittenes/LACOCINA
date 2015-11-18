0272a6453a02cd5ff2005e88253acd691b9512f4
ittenes <antonioriver@gmail.com>
2015-11-18 09:59:38 +0100

    Custom_fields
    tray to show the custom fields into the theme

--- index.php
+++ index.php
@@ -29,8 +29,12 @@
 				<div class="posts" id="posts">
 						
 			    	<?php while (have_posts()) : the_post(); ?>
-			    	
+
 			    		<?php get_template_part( 'content', get_post_format() ); ?>
+
+			    		<p>
+			    			<?php the_field('texto'); ?>
+			    		</p>
 			    			        		            
 			        <?php endwhile; ?>
 		        	                    
